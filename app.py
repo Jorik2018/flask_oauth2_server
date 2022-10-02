95a8e24f95f3656a3cecd9744bdf952b4eaa959a
Erik Antonio Alarcon Pinedo <ealarconp@regionancash.gob.pe>
2022-10-01 22:30:44 -0500

    change to sqlite

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = create_app({
     'SECRET_KEY': 'secret',
     'OAUTH2_REFRESH_TOKEN_GENERATOR': True,
     'SQLALCHEMY_TRACK_MODIFICATIONS': False,
-    'SQLALCHEMY_DATABASE_URI-':'sqlite:///db.sqlite',
-    'SQLALCHEMY_DATABASE_URI':'mysql+mysqlconnector://root:root@localhost/production'
+    'SQLALCHEMY_DATABASE_URI':'sqlite:///db.sqlite',
+    'SQLALCHEMY_DATABASE_URI/':'mysql+mysqlconnector://root:root@localhost/production'
 })
